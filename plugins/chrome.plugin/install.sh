0f454eb75d697110f2c4cc2cbd2833dbc7e90076
specialworld83 <specialworld83@gmail.com>
2019-03-12 09:23:55 +0100

    Add new plugin and fix chrome beta install

--- plugins/chrome.plugin/install.sh
+++ plugins/chrome.plugin/install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-pacman -S --noconfirm google-chrome
+if [ "$XDG_CURRENT_DESKTOP" = "KDE" ];then
+    konsole -e 'trizen -S --noconfirm google-chrome-beta'
+elif [ "$XDG_CURRENT_DESKTOP" = "GNOME" ];then
+    gnome-terminal -e 'trizen -S --noconfirm google-chrome-beta'
+elif [ "$XDG_CURRENT_DESKTOP" = "XFCE" ];then
+    xfce4-terminal -e 'trizen -S --noconfirm google-chrome-beta'
+elif [ "$XDG_CURRENT_DESKTOP" = "X-Cinnamon" ];then
+    gnome-terminal -e 'trizen -S --noconfirm google-chrome-beta'
+elif [ "$XDG_CURRENT_DESKTOP" = "MATE" ];then
+    mate-terminal -e 'trizen -S --noconfirm google-chrome-beta'
+fi
